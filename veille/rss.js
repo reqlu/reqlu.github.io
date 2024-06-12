f328806dfe940aa683e937d97a63d9ce69efd29e
reqlu <144426077+reqlu@users.noreply.github.com>
2024-06-12 21:59:43 +0200

    Update rss.js

--- veille/rss.js
+++ veille/rss.js
@@ -1,27 +1,35 @@
-  $(document).ready(function() {
-    // URL des flux RSS que vous avez collectés
-    var rssFeeds = [
-      'https://cert.ssi.gouv.fr/avis/feed/',
-      // Ajoutez autant de flux RSS que nécessaire
-    ];
+$(document).ready(function() {
+    var rssFeeds = {
+        avis: 'https://cert.ssi.gouv.fr/avis/feed/',
+        cti: 'https://www.cert.ssi.gouv.fr/cti/feed/'
+    };
 
-    rssFeeds.forEach(function(feedUrl) {
-      $.ajax({
-        url: 'https://api.rss2json.com/v1/api.json',
-        method: 'GET',
-        dataType: 'json',
-        data: {
-          rss_url: feedUrl
-        },
-        success: function(data) {
-          // Traitez les données du flux RSS ici et affichez-les dans le tableau
-          data.items.forEach(function(item) {
-            // Vérifiez si l'élément a une image
-            var image = '<img src="https://www.ginjfo.com/wp-content/uploads/2017/05/ANSI.jpg" alt="Image par défaut" />';
-            var titleWithImage = ' <a href="' + item.link + '">' + image + '<span>' + item.title + '</span></a>';
-            $('#feed-table tbody').append('<tr><td class="image-cell title-cell">' + titleWithImage + '</td></tr>');
-          });
-        }
-      });
-    });
-  });
+    function fetchRSS(feedUrl, tableId) {
+        $.ajax({
+            url: 'https://api.allorigins.win/get',
+            method: 'GET',
+            dataType: 'json',
+            data: {
+                url: feedUrl
+            },
+            success: function(data) {
+                var parser = new DOMParser();
+                var xmlDoc = parser.parseFromString(data.contents, "text/xml");
+                var items = xmlDoc.getElementsByTagName("item");
+                for (var i = 0; i < Math.min(items.length, 7); i++) { // Limite à 7 liens
+                    var title = items[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+                    var link = items[i].getElementsByTagName("link")[0].childNodes[0].nodeValue;
+                    var image = '<img src="https://www.ginjfo.com/wp-content/uploads/2017/05/ANSI.jpg" alt="Image par défaut" />';
+                    var titleWithImage = '<a href="' + link + '">' + image + '<span>' + title + '</span></a>';
+                    $('#' + tableId + ' tbody').append('<tr><td class="image-cell title-cell">' + titleWithImage + '</td></tr>');
+                }
+            },
+            error: function(xhr, status, error) {
+                console.error('Erreur AJAX: ' + error);
+            }
+        });
+    }
+
+    fetchRSS(rssFeeds.avis, 'feed-table-avis');
+    fetchRSS(rssFeeds.cti, 'feed-table-cti');
+});
